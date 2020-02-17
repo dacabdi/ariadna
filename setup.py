1435aea2d3847ba2c749eb5c62f6f5db0d7cea7e
David Cabrera <dacabdi@gmail.com>
2020-02-16 22:31:08 -0800

    create pylintrc and fix setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,34 @@
+""" Setup ariadna module """
+
 from setuptools import setup
 
 with open('README.md', 'r') as fh:
-    long_description = fh.read()
+    LONG_DESCRIPTION = fh.read()
 
 setup(
-    name = 'ariadna',
-    packages = ['ariadna'],
-    version = '0.0.2b',
-    license = 'MIT',
-    description = 'A library of featureful collection objects providing path-like keys',
-    long_description = long_description,
-    long_description_content_type = 'text/markdown',
-    author = 'David Cabrera',
-    author_email = 'dacabdi@gmail.com',
-    url = 'https://github.com/dacabdi/ariadna',
-    download_url = 'https://github.com/dacabdi/ariadna/archive/v0.0.2b.tar.gz',
-    keywords = [
-      'caja',
-      'dict',
-      'sequence',
-      'path',
-      'descriptor',
-      'set'
+    name='ariadna',
+    packages=['ariadna'],
+    version='0.0.2b',
+    license='MIT',
+    description='A library of featureful collection objects providing path-like keys',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    author='David Cabrera',
+    author_email='dacabdi@gmail.com',
+    url='https://github.com/dacabdi/ariadna',
+    download_url='https://github.com/dacabdi/ariadna/archive/v0.0.2b.tar.gz',
+    keywords=[
+        'caja',
+        'dict',
+        'sequence',
+        'path',
+        'descriptor',
+        'set'
     ],
-    install_requires = [
+    install_requires=[
         'frozendict'
     ],
-    classifiers = [
+    classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3 :: Only',
